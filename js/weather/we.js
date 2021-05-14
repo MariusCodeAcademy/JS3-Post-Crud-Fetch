3ae47ca3114e35e9d65ac25967491dfb3ee7e93d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-14 14:45:03 +0300

    oras

--- js/weather/we.js
+++ js/weather/we.js
@@ -1,12 +1,13 @@
 "use strict"; // here we go again
 import { appId } from "./vars.js";
-const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+// const weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
 const city = "kaunas";
+const weatherUrl = "/js/weather/sample.json";
+
 async function getWetherData() {
-  //   const url = "/js/weather/sample.json";
   const url = `${weatherUrl}?q=${city}&units=metric&appid=${appId}`;
   const resp = await fetch(url);
-  const data = await resp.text();
+  const data = await resp.json();
   return data;
 }
 
